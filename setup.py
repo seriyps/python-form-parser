1fb914299aa8fb9234d4af223112d115d667b88a
Sergey Prokhorov <sergey.prokhorov@ostrovok.ru>
2012-09-06 19:50:08 +0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+__version__ = '0.0.1'
+
+setup(
+    name = 'form-parser',
+    version = __version__,
+    description = 'Simple utilite/class for extract and fill HTML forms',
+    author = 'Sergey Prokhorov',
+    author_email = 'me@seriyps.ru',
+    py_modules = ['form_parser', ])
